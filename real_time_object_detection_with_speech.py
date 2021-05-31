a5c0c1caed7ba852506079d46728f09604ed2360
Namrata Chavhan <59946920+namratachavhan@users.noreply.github.com>
2021-05-31 13:55:35 +0530

    Add files via upload

---
+++ real_time_object_detection_with_speech.py
@@ -0,0 +1,108 @@
+#importing libraries
+from imutils.video import VideoStream
+from imutils.video import FPS
+from gtts import gTTS 
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+#passing the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+#defining the classes for object detection
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+#loading our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+#initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	
+	net.setInput(blob)
+	detections = net.forward()
+
+	#loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# extract the index of the class label from the
+			# `detections`, then compute the (x, y)-coordinates of
+			# the bounding box for the object
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	#if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+#text to speech conversion	
+text_val = label
+language = "en"
+obj = gTTS(text=text_val, lang=language, slow=False)
+#saving audio file in mp3 format
+obj.save("demo.mp3")  
+os.system("demo.mp3") 
+
+#Stop the timer and display the information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+#display the detected object with confidence value
+print("The object is : ",label)
+
+
+cv2.destroyAllWindows()
+vs.stop()
+
+#usage
+#python real_time_object_detection_with_speech.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+
+
